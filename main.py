57a05d6cacb4dec21e5d08f03833a33d5605f72c
m1504 <m15043340061@163.com>
2022-08-20 18:24:38 +0800

    Delete licenses function

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ cached = {}
 is_login = False
 
 print("LBCC v1.0.0-dev.")
-print("Type \"help\" or \"license\" for more information.")
+print("Type \"help\" for more information.")
 
 
 def get_login_status():
@@ -110,7 +110,7 @@ def login():
         print("你已登录!")
         return
     while True:
-        choose = input("选择登录方式:")
+        choose = input("选择登录方式: ")
         if choose == "password":
             validate, seccode, key, challenge = verify_captcha_key()
             username = input("用户名: ")
@@ -476,30 +476,6 @@ def comment_viewer(aid):
                 break
 
 
-def licenses():
-    print("""MIT License
-
-Copyright (c) 2022 Laosun Studios.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.""")
-
-
 def clean_cache():
     shutil.rmtree("cached")
     os.mkdir("cached")
@@ -564,6 +540,7 @@ def recommend():
             bvid, _, view, danmaku, like_, coin, favorite, share, comment_count = video_status(
                 i['bvid'])
             print("标题: ", i['title'])
+            print("作者: ", i['owner']['name'])
             print("bvid: ", bvid)
             print("观看量: ", view)
             print("标签: ", ", ".join(get_tag(avid, cid)))
@@ -747,8 +724,6 @@ while True:
         break
     elif choose1 == "help":
         main_help()
-    elif choose1 == 'license':
-        licenses()
     elif choose1 == "clean_cache":
         clean_cache()
         print("成功清除缓存!")
