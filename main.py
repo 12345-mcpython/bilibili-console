a76f11c3d6e6f06e4d9b59ea7e20e2b4fe8bc02e
m1504 <m15043340061@163.com>
2022-08-21 16:49:32 +0800

    Update LICENSE

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+The old version also use the GPL-3.0 license, not Mit License.
 """
 
 import shutil
