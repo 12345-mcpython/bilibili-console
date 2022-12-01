9dc7e3610250a899e6898623023db29f824c8ebb
m1504 <m15043340061@163.com>
2022-12-01 12:00:44 +0800

    :sparkles: feature: add people watching count,add danmaku and rewrite video play.

--- main.py
+++ main.py
@@ -25,10 +25,14 @@ The old version also use the GPL-3.0 license, not MIT License.
 """
 import datetime
 import os
+import subprocess
+import sys
+import time
 
 import requests
 from requests.utils import dict_from_cookiejar
 
+from bilibili.biliass import Danmaku2ASS
 from bilibili.utils import format_time
 
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -60,6 +64,7 @@ class BiliBili:
     def recommend(self):
         print("推荐界面")
         while True:
+            # no cache
             recommend_request = self.get("https://api.bilibili.com/x/web-interface/index/top/feed/rcmd?ps=5")
             for num, item in enumerate(recommend_request.json()['data']['item']):
                 print(num + 1, ":")
@@ -96,7 +101,18 @@ class BiliBili:
                 print("未知命令!")
 
     def main(self):
+        self.is_login()
         self.recommend()
+
+    def get_danmaku(self, cid):
+        params = {
+            'type': '1',
+            'oid': cid,
+            'segment_index': '1'
+        }
+        # cache
+        resp = self.get("https://api.bilibili.com/x/v2/dm/web/seg.so", params=params, cache=True)
+        return resp.content
 
     def choose_video(self, video_id, bvid=True, title=""):
         print("\n")
@@ -105,7 +121,8 @@ class BiliBili:
             url = "https://api.bilibili.com/x/web-interface/view/detail?bvid=" + video_id
         else:
             url = "https://api.bilibili.com/x/web-interface/view/detail?aid=" + video_id
-        r = self.get(url, no_cache=False)
+        # cache
+        r = self.get(url, cache=True)
         video = r.json()['data']["View"]["pages"]
         for i in video:
             print(f"{i['page']}: {i['part']}")
@@ -126,7 +143,8 @@ class BiliBili:
             break
         return
 
-    def play(self, video_id, cid, bvid=True, title="", danmaku=False):
+    def play(self, video_id, cid, bvid=True, title="", danmaku=False, width=0, height=0):
+        # width height 参数用于bangumi
         if danmaku:
             # danmaku 不需video_id, bvid
             url = f"https://api.bilibili.com/pgc/player/web/playurl?cid={cid}&fnval=16&qn={self.quality}"
@@ -135,42 +153,89 @@ class BiliBili:
                 url = f"https://api.bilibili.com/x/player/playurl?cid={cid}&bvid={video_id}&fnval=16"
             else:
                 url = f"https://api.bilibili.com/x/player/playurl?cid={cid}&avid={video_id}&fnval=16"
-        play_url_request = self.get(url)
-        audio_url = ""
-        play_url = ""
-        if danmaku:
-            play_url = play_url_request.json()['result']['durl'][0]['url']
+        # cache
+        play_url_request = self.get(url, cache=True)
+        if not danmaku:
+            videos = play_url_request.json()['data']['dash']["video"]
+            audios = play_url_request.json()['data']['dash']["audio"]
 
+            video_mapping = {}
+            audio_mapping = {}
+
+            for i in videos:
+                if i['codecs'].startswith('avc'):
+                    video_mapping[i['id']] = {"id": i['id'], "url": i['base_url'], "width": i['width'],
+                                              "height": i['height']}
+
+            for i in audios:
+                audio_mapping[i['id']] = i['base_url']
+
+            default_audio = sorted(list(audio_mapping.keys()), reverse=True)[0]
+            default_video = sorted(list(video_mapping.keys()), reverse=True)[0]
+
+            try:
+                audio_url = audio_mapping[self.audio]
+            except KeyError:
+                audio_url = audio_mapping[default_audio]
+            try:
+                video_url = video_mapping[self.quality]['url']
+                width = video_mapping[self.quality]['width']
+                height = video_mapping[self.quality]['height']
+            except KeyError:
+                video_url = video_mapping[default_video]['url']
+                width = video_mapping[default_video]['width']
+                height = video_mapping[default_video]['height']
         else:
-            for url_ in play_url_request.json()['data']['dash']['video']:
-                if url_['id'] == self.quality and url_['codecs'].startswith(self.codecs):
-                    play_url = url_['base_url']
-                    break
-            for audio_ in play_url_request.json()['data']['dash']['audio']:
-                if audio_['id'] == self.audio:
-                    audio_url = audio_['base_url']
-                    break
-
+            video_url = play_url_request.json()['result']['durl'][0]['url']
+            audio_url = video_url
+        a = Danmaku2ASS(
+            self.get_danmaku(cid),
+            width,
+            height,
+            input_format="protobuf",
+            reserve_blank=0,
+            font_face="SimHei",
+            font_size=25,
+            text_opacity=0.8,
+            duration_marquee=15.0,
+            duration_still=10.0,
+            comment_filter=None,
+            is_reduce_comments=False,
+            progress_callback=None,
+        )
+        with open(f"cached/{cid}.ass", "w", encoding="utf-8") as f:
+            f.write(a)
         command = f"mpv " \
                   f"--sub-file=\"cached/{cid}.ass\" " \
                   f"--user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
                   f"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53\" " \
-                  f"--referrer=\"https://www.bilibili.com\" \"{play_url}\" " \
+                  f"--referrer=\"https://www.bilibili.com\" \"{video_url}\" " \
                   f"--audio-file=\"{audio_url}\" " \
                   f"--title=\"{title}\""
-        os.system(command)
+        p = subprocess.Popen(command, shell=True)
+        while p.poll() is None:
+            if bvid:
+                people_watching = self.get(f"https://api.bilibili.com/x/player/online/total?cid={cid}&bvid={video_id}")
+            else:
+                people_watching = self.get(f"https://api.bilibili.com/x/player/online/total?cid={cid}&aid={video_id}")
+            people = f"\r{people_watching.json()['data']['total']} 人正在看"
+            print(people, end="", flush=True)
+            time.sleep(2)
+        print("\n")
 
     def is_login(self):
+        # no cache
         r = self.get('https://api.bilibili.com/x/member/web/account')
         if r.json()['code'] == -101:
             return False
         elif r.json()['code'] == 0:
+            # 登录才可使用32 80分辨率 大会员分辨率暂不支持
             self.quality = 80
             return True
         else:
             return None
 
-    def get(self, url: str, params=None, no_cache=True, **kwargs) -> requests.Response:
+    def get(self, url: str, params=None, cache=False, **kwargs) -> requests.Response:
         if self.cached_response.get(url):
             return self.cached_response.get(url)
         else:
@@ -184,7 +249,7 @@ class BiliBili:
                     count -= 1
                     if count <= 0:
                         raise request_error
-            if not no_cache:
+            if cache:
                 self.cached_response[url] = r
             return r
 
